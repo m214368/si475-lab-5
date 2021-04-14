1429b491e1217d6878ae4a087b030d3fe451d3c8
m214368 <73185501+m214368@users.noreply.github.com>
2021-04-14 13:59:03 -0400

    a

--- realDemo.py
+++ realDemo.py
@@ -236,6 +236,20 @@ def Blob():
     else :
         detector = cv2.SimpleBlobDetector_create(params)
 
+    blank = np.zeros((1, 1)) 
+    blobs = cv2.drawKeypoints(image, keypoints, blank, (0, 0, 255),
+                              cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+  
+    number_of_blobs = len(keypoints)
+    text = "Number of Circular Blobs: " + str(len(keypoints))
+    cv2.putText(blobs, text, (20, 550),
+    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 100, 255), 2)
+  
+    # Show blobs
+    cv2.imshow("Filtering Circular Blobs Only", blobs)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 #color = raw_input("What color to hunt:")
 color = "purple"
 hunt(color)
